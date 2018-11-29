fa7ef083ec52860fe383a826c7eed3505006fb84
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2018-11-29 12:05:38 -0500

    Updated the Chat Activity
    Added a new Drawable file, which makes the send arrow dark
    
    Added a toast for a fail login, Still need to work on that

--- app/src/main/java/net/uoit/csci4100/mobiledeviceproject/LoginActivity.java
+++ app/src/main/java/net/uoit/csci4100/mobiledeviceproject/LoginActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -46,7 +48,7 @@ public class LoginActivity extends AppCompatActivity  {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(!task.isSuccessful()){
-
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
                 }else{
                     Intent launchChat = new Intent(LoginActivity.this, MainActivity.class );
                     startActivityForResult(launchChat,CHAT);
