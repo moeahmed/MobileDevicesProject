a29f57d5c04373f1a8bcfb7bb9a923e1abef5859
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2018-11-29 08:16:06 -0500

    Added A few things
    First I added a Chat UI Which still need work on it,
    Second, I played around the Auth with Username and password,
    Third, Added a way to updated the firebase through code

--- app/src/main/java/net/uoit/csci4100/mobiledeviceproject/LoginActivity.java
+++ app/src/main/java/net/uoit/csci4100/mobiledeviceproject/LoginActivity.java
@@ -1,13 +1,60 @@
 package net.uoit.csci4100.mobiledeviceproject;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity  {
+
+    private FirebaseAuth mAuth;
+    public static final int CHAT = 0;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null){
+                    Intent launchChat = new Intent(LoginActivity.this, MainActivity.class );
+                    startActivityForResult(launchChat,CHAT);
+                }
+            }
+        };
     }
+
+    public void handleLogin(View view){
+        EditText txtUser = findViewById(R.id.txtUsername);
+        EditText txtPass = findViewById(R.id.txtPassword);
+
+        mAuth.signInWithEmailAndPassword(txtUser.getText().toString(),
+                                        txtPass.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+
+                }else{
+                    Intent launchChat = new Intent(LoginActivity.this, MainActivity.class );
+                    startActivityForResult(launchChat,CHAT);
+                }
+            }
+        });
+
+    }
+
+
 }
--- app/src/main/java/net/uoit/csci4100/mobiledeviceproject/MainActivity.java
+++ app/src/main/java/net/uoit/csci4100/mobiledeviceproject/MainActivity.java
@@ -1,5 +1,7 @@
 package net.uoit.csci4100.mobiledeviceproject;
 
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,17 +10,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TableLayout;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private TabLayout mTabLayout;
     private AppBarLayout mAppBarLayout;
     private ViewPager mViewPager;
+    private DatabaseReference mRef;
+    private FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        mRef = FirebaseDatabase.getInstance().getReference();
+
+        mRef.child("Accounts").child(mUser.getUid()).child("Name").setValue("Somthing");
+
+        mRef.child("Accounts").child(mUser.getUid()).child("Name").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Toast.makeText(MainActivity.this, dataSnapshot.getValue(String.class), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 1000);
 
         // Initialize AppBar and components
         mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
