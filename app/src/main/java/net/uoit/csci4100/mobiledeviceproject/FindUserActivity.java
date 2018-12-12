34e937418d1f220963b1199ad80b132cbcaac114
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2018-12-12 15:52:03 -0500

    Bug Fix:
    -Fixed when adding someone from Contacts, the name should be the name rather then email
    -Fixed when a new user is entering info, especially adding the picture first, it will update the db with the picture
    -Fixed overriding issue caused by the Update Profile
    -When adding the contact, it will redirect you to the main activity
    
    My plan is to add an image cropper since the current images have different ratio, causing some disruption in the layout

--- app/src/main/java/net/uoit/csci4100/mobiledeviceproject/FindUserActivity.java
+++ app/src/main/java/net/uoit/csci4100/mobiledeviceproject/FindUserActivity.java
@@ -1,5 +1,6 @@
 package net.uoit.csci4100.mobiledeviceproject;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,9 +10,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -22,6 +27,9 @@ public class FindUserActivity extends AppCompatActivity {
     private RecyclerView mRecylerView;
 
     private DatabaseReference mRef;
+    private DatabaseReference mContactRef;
+
+    private String currentUserID;
 
 
     @Override
@@ -30,6 +38,10 @@ public class FindUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_find_user);
 
         mRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        mContactRef = FirebaseDatabase.getInstance().getReference().child("Contacts");
+
+        currentUserID = FirebaseAuth.getInstance().getUid();
+
 
         mRecylerView = (RecyclerView)findViewById(R.id.finduser_Recycler);
         mRecylerView.setLayoutManager(new LinearLayoutManager(this));
@@ -50,11 +62,25 @@ public class FindUserActivity extends AppCompatActivity {
 
         FirebaseRecyclerAdapter<Users, UserListAdapter> adapters = new FirebaseRecyclerAdapter<Users, UserListAdapter>(opt) {
             @Override
-            protected void onBindViewHolder(@NonNull UserListAdapter holder, int position, @NonNull Users model) {
+            protected void onBindViewHolder(@NonNull UserListAdapter holder, final int position, @NonNull Users model) {
 
                 holder.userName.setText(model.getName());
                 holder.userEmail.setText(model.getEmail());
                 Picasso.get().load(model.getImage()).placeholder(R.drawable.avatar1).into(holder.userProfileImage);
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String visitUserID = getRef(position).getKey();
+
+                        Toast.makeText(FindUserActivity.this, "Added user to contacts", Toast.LENGTH_LONG).show();
+                        accceptChat(visitUserID);
+
+                        Intent mainIntent = new Intent(FindUserActivity.this, MainActivity.class);
+                        startActivity(mainIntent);
+                    }
+                });
+
             }
 
             @NonNull
@@ -69,6 +95,34 @@ public class FindUserActivity extends AppCompatActivity {
 
         mRecylerView.setAdapter(adapters);
         adapters.startListening();
+
+
+
+    }
+
+    private void accceptChat(final String otherUserID){
+        
+        if(currentUserID == otherUserID){
+            Toast.makeText(this, "You Can Not Add YOUR SELF!", Toast.LENGTH_SHORT).show();
+        }else{
+            mContactRef.child(otherUserID).child(currentUserID).child("Contacts").setValue("Saved")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                mContactRef.child(currentUserID).child(otherUserID).child("Contacts").setValue("Saved")
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()){
+
+                                                }
+                                            }
+                                        });
+                            }
+                        }
+                    });
+        }
 
 
 
