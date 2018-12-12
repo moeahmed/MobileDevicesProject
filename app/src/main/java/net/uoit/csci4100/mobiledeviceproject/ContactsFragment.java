9579b4b7063c399beab3f95673bd11d7a79483e1
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2018-12-12 03:57:36 -0500

    Now completed the Contact list
    All you do is, click on the person name in Find User, and it will add it in the contact

--- app/src/main/java/net/uoit/csci4100/mobiledeviceproject/ContactsFragment.java
+++ app/src/main/java/net/uoit/csci4100/mobiledeviceproject/ContactsFragment.java
@@ -2,18 +2,36 @@ package net.uoit.csci4100.mobiledeviceproject;
 
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ContactsFragment extends Fragment {
 
+    private View mContactView;
+    private RecyclerView mContactList;
 
+    private DatabaseReference mRef;
+    private DatabaseReference mUsersRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
     public ContactsFragment() {
         // Required empty public constructor
     }
@@ -23,9 +41,63 @@ public class ContactsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        mContactView =inflater.inflate(R.layout.fragment_contacts, container, false);
+        mContactList = (RecyclerView) mContactView.findViewById(R.id.userList);
+        mContactList.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getUid();
+        mRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentUserID);
+        mUsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        return mContactView;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Users>()
+                .setQuery(mRef, Users.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Users, UserListAdapter> adapter = new FirebaseRecyclerAdapter<Users, UserListAdapter>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final UserListAdapter holder, int position, @NonNull final Users model) {
+
+                String userID = getRef(position).getKey();
+                mUsersRef.child(userID).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild("image")){
+                            String profileImage = dataSnapshot.child("image").getValue().toString();
+                            Picasso.get().load(profileImage).placeholder(R.drawable.avatar1).into(holder.userProfileImage);
+                        }
+                        String profileName = dataSnapshot.child("name").getValue().toString();
+                        String profileEmail = dataSnapshot.child("email").getValue().toString();
+
+                        holder.userName.setText(profileName);
+                        holder.userName.setText(profileEmail);
+                        //Picasso.get().load().placeholder(R.drawable.avatar1).into(holder.userProfileImage);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public UserListAdapter onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.userdisplay_layout, viewGroup,false );
+                UserListAdapter userListAdapter = new UserListAdapter(view);
+
+                return userListAdapter;
+            }
+        };
+
+        mContactList.setAdapter(adapter);
+        adapter.startListening();
+    }
 }
